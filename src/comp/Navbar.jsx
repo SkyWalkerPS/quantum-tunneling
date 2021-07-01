7b8c17680cc48efaca2dd17cd45af6afbb193c1c
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-07-01 14:22:43 +0530

    add navbar

--- src/comp/Navbar.jsx
+++ src/comp/Navbar.jsx
@@ -1,10 +1,20 @@
 import "./Navbar.scss";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-
-  return (
-    <div className="navbar">Navbar</div>
-  )
+    const location = useLocation();
+    console.log(location);
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <h2>Quantum Tunneling Simulator</h2>
+            </div>
+            <ul className="navbar-links">
+                <li><Link to="/" className={location.pathname==="/" && "active"}>Home</Link></li>
+                <li><Link to="/about" className={location.pathname==="/about" && "active"}>About</Link></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar;
